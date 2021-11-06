dbc1c3a6b14b3ed433c21e8c350236fff2e2b3c7
Test Projects 83 <testprojects83@gmail.com>
2021-11-06 21:09:24 +0000

    minor change
    Co-Authored-By: Miracle Ogunleye <60979917+mimiicodes@users.noreply.github.com>

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,38 +1,29 @@
 import React, { useState } from 'react';
 import './MainPage.css';
-import logo from '././assets/logo.png';
+import logo from '../logo.png';
 
 const MainPage = () => {
-  // Default state
-  const [textEntered, settextEntered] = useState(true);
-  const [islogo, setIsLogo] = useState(' ');
+  // default item in memory
+  const [textEntered, setTextEntered] = useState('');
 
-  // Defining the textHandler function
   const textHandler = (e) => {
-    setIsLogo(e.target.value);
-    settextEntered(false);
-
-    // Removing any whitespace if the search box is empty
-    if (e.target.value.trim().length === 0) {
-      settextEntered(true);
-    }
+    setTextEntered(e.target.value);
   };
 
   return (
     <div className='mainPage'>
       <div className='content__wrapper'>
-        {/* Checking if the input is true, replace the input texts with the Side Hustle Logo then vice versa */}
         {textEntered ? (
-          <img src={logo} alt='side hustle logo' className='logo' />
+          <p className='edited__text'>{textEntered}</p>
         ) : (
-          <p>{islogo}</p>
+          <img src={logo} alt='side hustle logo' className='logo' />
         )}
         <div className='input__wrapper'>
           <input
             type='text'
             placeholder='Enter something meaningful'
             className='searchInput'
-            onchange={textHandler}
+            onChange={textHandler}
           />
         </div>
         <div className='mainBtn'>
@@ -43,10 +34,10 @@ const MainPage = () => {
           <p className='main__p'>
             Google offered in:
             <span>
-              <a href='/'>Hausa</a>
-              <a href='/'>Igbo</a>
-              <a href='/'>Èdè Yorùbá</a>
-              <a href='/'>Nigerian Pidgin</a>
+              <a href='#'>Hausa</a>
+              <a href='#'>Igbo</a>
+              <a href='#'>Èdè Yorùbá</a>
+              <a href='#'>Nigerian Pidgin</a>
             </span>
           </p>
         </div>
